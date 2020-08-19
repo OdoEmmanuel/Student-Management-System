37898d6cd4eeee61e9b4ef146ef2c02eda423dec
emmanuel <odoemmanuel100@gmail.com>
2020-08-19 15:02:53 +0100

    ministatement correction

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -13,7 +14,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::all();
+        return view('student', ['students' => $students, 'layout' => 'index']);
     }
 
     /**
@@ -23,7 +25,9 @@ class StudentController extends Controller
      */
     public function create()
     {
-        //
+        $students = Student::all();
+        return view('student', ['students' => $students, 'layout' => 'create']);
+
     }
 
     /**
@@ -34,7 +38,15 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $student = new Student();
+        $student->cne = $request->input('cne');
+        $student->firstName = $request->input('firstName');
+        $student->secondName = $request->input('secondName');
+        $student->age = $request->input('age');
+        $student->specialty = $request->input('specialty');
+        $student->save();
+        return redirect('/');
+
     }
 
     /**
@@ -45,7 +57,9 @@ class StudentController extends Controller
      */
     public function show($id)
     {
-        //
+        $student = Student::find($id);
+        $students = Student::all();
+        return view('student', ['students' => $students, 'student' => $student, 'layout' => 'show']);
     }
 
     /**
@@ -56,7 +70,12 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
-        //
+        $student = Student::find($id);
+        $students = Student::all();
+        return view('student', ['students' => $students, 'student' => $student, 'layout' => 'edit']);
+
+
+
     }
 
     /**
@@ -68,7 +87,14 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $student = Student::find($id);
+        $student->cne = $request->input('cne');
+        $student->firstName = $request->input('firstName');
+        $student->secondName = $request->input('secondName');
+        $student->age = $request->input('age');
+        $student->specialty = $request->input('specialty');
+        $student->save();
+        return redirect('/');
     }
 
     /**
@@ -79,6 +105,8 @@ class StudentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $student = Student::find($id);
+        $student->delete();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', "StudentController@index");
+Route::get('/edit/{id}', "StudentController@edit");
+Route::get('/show/{id}', "StudentController@show");
+Route::get('/create', "StudentController@create");
+Route::post('/store', "StudentController@store");
+Route::post('/update/{id}', "StudentController@update");
+
+
